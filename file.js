fdd0183a5983f6ddcab603f60bc94ac2ae2eb6c8
TETIANAS09 <150555108+TETIANAS09@users.noreply.github.com>
2024-02-11 08:45:40 +0100

    Add files via upload

---
+++ file.js
@@ -0,0 +1,36 @@
+/*Створіть функцію-конструктор Calculator, який створює об’єкти з трьома
+методами:
+read() запитує два значення за допомогою prompt і запам’ятовує їх у
+властивостях об’єкта.
+sum() повертає суму цих властивостей.
+mul() повертає результат множення даних властивостей.
+*/
+
+///result///
+/*
+class Calculator {
+  constructor() {
+    this.a = 0;
+    this.b = 0;
+  }
+
+  read() {
+    this.a = parseFloat(prompt("ВВЕДІТЬ ПЕРШЕ ЧИСЛО:", "0"));
+    this.b = parseFloat(prompt("ВВЕДІТЬ ДУГЕ ЧИСЛО:", "0"));
+  }
+
+  sum() {
+    return this.a + this.b;
+  }
+
+  mul() {
+    return this.a * this.b;
+  }
+}
+
+const calculator = new Calculator();
+calculator.read();
+console.log("Сума:", calculator.sum());
+console.log("Множення:", calculator.mul());
+
+*/
